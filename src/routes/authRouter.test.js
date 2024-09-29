f88f7ca95d924c309d6169bbd6cbe1ec51d72b03
Jisu Song <songjisu2487@gmail.com>
2024-09-28 19:15:27 -0600

    updated ci pipeline

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -1,20 +1,50 @@
 const request = require('supertest');
 const app = require('../service');
+const { setAuthUser } = require('./authRouter');
 
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
 let testUserAuthToken;
 
 beforeAll(async () => {
     testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+    testUser.name = Math.random().toString(36).substring(2, 12);
+    testUser.password = Math.random().toString(36).substring(2, 12);
     const registerRes = await request(app).post('/api/auth').send(testUser);
     testUserAuthToken = registerRes.body.token;
 })
 
-test('login', async () => {
+test('login and logout', async () => {
     const loginRes = await request(app).put('/api/auth').send(testUser);
     expect(loginRes.status).toBe(200);
     expect(loginRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
 
     const { password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
     expect(loginRes.body.user).toMatchObject(user);
+
+    const logoutRes = await request(app).delete('/api/auth').set('Authorization', `Bearer ${testUserAuthToken}`);
+    expect(logoutRes.status).toBe(200);
 });
+
+test('setAuthUser line 45-52', async () => {
+    const loginRes = await request(app).put('/api/auth').send(testUser);
+    expect(loginRes.status).toBe(200);
+
+    const token = loginRes.body.token; 
+
+    const req = {
+        headers: {
+            authorization: `Bearer ${token}`,
+        },
+        user: null, // This will be set by setAuthUser
+    };
+
+    const res = {};
+    const next = jest.fn();
+    await setAuthUser(req, res, next);
+
+    expect(req.user.name).toBeDefined();
+    expect(req.user.isRole).toBeDefined();
+
+    expect(next).toHaveBeenCalled();
+});
+
